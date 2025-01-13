94435ef423feb7a14945ea4ddf15a3c054c7c7a4
André Willyan <andrewillyan@tutanota.com>
2025-01-12 23:38:00 -0300

    add basic ansible setup to copy the topology to the managed nodes

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,10 +3,13 @@
 source env.sh
 
 # setup isolated project
-echo "Creating" $LXC_PROJECT_NAME "project" 
-lxc project create $LXC_PROJECT_NAME
-lxc profile device add default root disk path=/ pool=default --project $LXC_PROJECT_NAME
-lxc profile device add default eth0 nic name=eth0 nictype=p2p --project $LXC_PROJECT_NAME
+if ! lxc project list --format=csv | cut -d ',' -f 1 | grep -q "$LXC_PROJECT_NAME"
+then
+    echo "Creating" $LXC_PROJECT_NAME "project" 
+    lxc project create $LXC_PROJECT_NAME
+    lxc profile device add default root disk path=/ pool=default --project $LXC_PROJECT_NAME
+    lxc profile device add default eth0 nic name=eth0 nictype=p2p --project $LXC_PROJECT_NAME
+fi
 
 # create node containers
 for i in {0..9}
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -4,6 +4,6 @@ source env.sh
 
 for i in {0..9}
 do
-    echo "Starting n$i"
+    echo "Stopping n$i"
     lxc stop n$i --project $LXC_PROJECT_NAME
 done
