854baa26190fdf8249691180a1531d15f87a0c9c
André Willyan <andrewillyan@tutanota.com>
2025-01-13 03:07:22 -0300

    spawn gRPC clients and servers

---
+++ src/log.rs
@@ -0,0 +1,22 @@
+use chrono::Local;
+use tokio::fs::File;
+use tokio::io::AsyncWriteExt;
+
+pub struct Logger {
+    log_sink: File,
+}
+
+impl Logger {
+    pub fn new(sink: File) -> Self {
+        Self { log_sink: sink }
+    }
+}
+
+impl Logger {
+    pub async fn log(&mut self, msg: &str) -> Result<(), std::io::Error> {
+        let now = Local::now();
+        let log_msg = format!("[{}] {}", now.format("%d-%m-%Y %H:%M:%S"), msg);
+        self.log_sink.write_all(log_msg.as_bytes()).await?;
+        Ok(())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,72 @@
-use chrono::Local;
-use std::{env, fs::File, io::Write};
+use greet::{
+    greeter_client::GreeterClient,
+    greeter_server::{Greeter, GreeterServer},
+    GreetRequest, GreetResponse,
+};
+use std::{collections::HashMap, env};
+use tokio::fs::File;
+use tonic::{transport::Server, Request, Response, Status};
+
+use crate::log::Logger;
+
+mod log;
+
+mod greet {
+    tonic::include_proto!("greet");
+}
+
+struct GreeterService;
+
+#[tonic::async_trait]
+impl Greeter for GreeterService {
+    async fn greet(&self, req: Request<GreetRequest>) -> Result<Response<GreetResponse>, Status> {
+        let name = req.into_inner().name;
+
+        let reply = GreetResponse {
+            text: format!("Hello ${name}!"),
+        };
+
+        Ok(Response::new(reply))
+    }
+}
 
 const PREFIX_PATH: &str = "/root";
+const GRPC_PORT: u32 = 50051;
 
-pub struct Logger<W> {
-    log_sink: W,
-}
-
-impl<W> Logger<W> {
-    pub fn new(sink: W) -> Self {
-        Self { log_sink: sink }
-    }
-}
-
-impl<W: Write> Logger<W> {
-    pub fn log(&mut self, msg: &str) -> Result<(), std::io::Error> {
-        let now = Local::now();
-        writeln!(
-            self.log_sink,
-            "[{}] {}",
-            now.format("%d-%m-%Y %H:%M:%S"),
-            msg
-        )?;
-        Ok(())
-    }
-}
-
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let hostname = env::var("HOSTNAME")?;
-    let log_file = File::create(format!("{PREFIX_PATH}/{hostname}.log"))?;
+    let log_file = File::create(format!("{PREFIX_PATH}/{hostname}.log")).await?;
     let mut logger = Logger::new(log_file);
 
-    logger.log(&format!("{hostname} started running."))?;
+    logger.log(&format!("{hostname} started running.")).await?;
+
+    let topology_file = std::fs::File::open(format!("{PREFIX_PATH}/topology.json"))?;
+    let topology: HashMap<String, Vec<String>> = serde_json::from_reader(topology_file)?;
+    let neighbors = topology
+        .get(&hostname)
+        .expect("Topology should list the neighbors of every node.");
+
+    logger
+        .log(&format!("neighbors = {}.", neighbors.join(",")))
+        .await?;
+
+    let addr = format!("[::1]:{GRPC_PORT}").parse()?;
+    let server = Server::builder().add_service(GreeterServer::new(GreeterService));
+
+    tokio::spawn(async move {
+        server
+            .serve(addr)
+            .await
+            .expect("Failed to start gRPC server.");
+    });
+
+    let mut clients = Vec::with_capacity(neighbors.len());
+    for n in neighbors {
+        let url = format!("http://{n}:{GRPC_PORT}");
+        // this way i can't create a clients concurrently, make it possible.
+        clients.push(GreeterClient::connect(url).await?);
+    }
+
     Ok(())
 }
