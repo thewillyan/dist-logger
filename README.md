94c3bcdabe5adc22c73fffe614b4e6c88b34f3ea
André Willyan <andrewillyan@tutanota.com>
2025-01-13 01:40:33 -0300

    setup basic project components

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # dist-logger
 
 1. Creates containers
-2. Pass to them their neigbors
-3. Make then write the time that they have alrived
+2. Pass to them their neighbors
+3. Make the managed notes write when they started to execute the program
 4. Make then say hello to the neighbors
-5. Make the neigbors write who greet them and the time of the greeting
+5. Make the neighbors write who greet them and when to the log
+6. Exit when all neighbors have been greeted and all neighbors have greeted me
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+use chrono::Local;
+use std::{env, fs::File, io::Write};
+
+const PREFIX_PATH: &str = "/root";
+
+pub struct Logger<W> {
+    log_sink: W,
+}
+
+impl<W> Logger<W> {
+    pub fn new(sink: W) -> Self {
+        Self { log_sink: sink }
+    }
+}
+
+impl<W: Write> Logger<W> {
+    pub fn log(&mut self, msg: &str) -> Result<(), std::io::Error> {
+        let now = Local::now();
+        writeln!(
+            self.log_sink,
+            "[{}] {}",
+            now.format("%d-%m-%Y %H:%M:%S"),
+            msg
+        )?;
+        Ok(())
+    }
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let hostname = env::var("HOSTNAME")?;
+    let log_file = File::create(format!("{PREFIX_PATH}/{hostname}.log"))?;
+    let mut logger = Logger::new(log_file);
+
+    logger.log(&format!("{hostname} started running."))?;
+    Ok(())
 }
